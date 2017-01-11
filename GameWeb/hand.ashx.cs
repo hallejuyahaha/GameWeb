b618546dd69c8ad3d35a4f56da1ef424497db3fb
hallejuyahaha <357563221@qq.com>
2017-01-12 00:26:49 +0800

    注册

--- GameWeb/hand.ashx.cs
+++ GameWeb/hand.ashx.cs
@@ -5,13 +5,13 @@ using System.Linq;
 using System.Web;
 
 namespace GameWeb
-{  
+{
     /// <summary>
     /// hand 的摘要说明
     /// </summary>
     public class hand : IHttpHandler
     {
-        
+
         public void ProcessRequest(HttpContext context)
         {
             string method = context.Request.Form["method"];
@@ -37,12 +37,33 @@ namespace GameWeb
                         context.Response.Write(result);
                     }
                     break;
+                case "indexAdd":
+                    {
+                        string fflag;
+                        string newna = context.Request.Form["newname"];
+                        string newpa = context.Request.Form["newpass"];
+                        DataTable indexSelect = Common.Excute.ExecuteQuery("select password,five,fivewin,bird from GameData where username = '" + newna + "'");
+                        if (indexSelect != null && indexSelect.Rows.Count > 0)
+                        {
+                            fflag = "失败";
+                            context.Response.ContentType = "text/plain";
+                            context.Response.Write(fflag);
+                        }
+                        else
+                        {
+                            fflag = "成功";
+                            Common.Excute.ExcuteCount("insert into GameData (username,password,five,fivewin,bird) values ('" + newna + "','" + newpa + "',0,0,0)");
+                            context.Response.ContentType = "text/plain";
+                            context.Response.Write(fflag);
+                        }
+                    }
+                    break;
                 case "five":
                     {
                         string nowusername = context.Request.Form["nowusername"];
-                       // DataTable five = Common.Excute.ExecuteQuery("select username,fivewin from GameData where fivewin = (select max(fivewin) from GameData)");
+                        // DataTable five = Common.Excute.ExecuteQuery("select username,fivewin from GameData where fivewin = (select max(fivewin) from GameData)");
                         DataTable five = Common.Excute.ExecuteQuery("select top 3 username,fivewin from GameData order by fivewin desc");
-                        DataTable nowuser = Common.Excute.ExecuteQuery("select fivewin from GameData where username = '"+nowusername +"'");
+                        DataTable nowuser = Common.Excute.ExecuteQuery("select fivewin from GameData where username = '" + nowusername + "'");
                         // string most = context.Request.Form["most"];
                         DataRow aa = nowuser.Rows[0];
                         string c = aa[0].ToString();
@@ -51,13 +72,13 @@ namespace GameWeb
                         string a1;
                         string a2;
                         string a3;
-                        string b ;
+                        string b;
 
                         a1 = five.Rows[0]["username"].ToString() + "`" + five.Rows[0]["fivewin"].ToString();
                         a2 = five.Rows[1]["username"].ToString() + "`" + five.Rows[1]["fivewin"].ToString();
                         a3 = five.Rows[2]["username"].ToString() + "`" + five.Rows[2]["fivewin"].ToString();
 
-                       // string d = a + "`" + b + "`" +c;
+                        // string d = a + "`" + b + "`" +c;
                         string d = a1 + "`" + a2 + "`" + a3 + "`" + c;
                         context.Response.ContentType = "text/plain";
                         context.Response.Write(d);
@@ -66,10 +87,10 @@ namespace GameWeb
                 case "fivewin":
                     {
                         Common.Excute.ExcuteCount("update GameData set fivewin = fivewin + 1 where username = '" + context.Request.Form["username"] + "'");
-                       // string username = context.Request.Form["username"];
-                       // DataTable fivewin = Common.Excute.ExecuteQuery("select fivewin from GameData where username = '"+username+"'");
-                       // DataRow fivewinRow = fivewin.Rows[0];
-                       // string newscore = fivewinRow["fivewin"].ToString();
+                        // string username = context.Request.Form["username"];
+                        // DataTable fivewin = Common.Excute.ExecuteQuery("select fivewin from GameData where username = '"+username+"'");
+                        // DataRow fivewinRow = fivewin.Rows[0];
+                        // string newscore = fivewinRow["fivewin"].ToString();
                         context.Response.ContentType = "text/plain";
                         context.Response.Write("");
                     }
@@ -81,7 +102,7 @@ namespace GameWeb
                     break;
                 default: break;
             }
-            
+
         }
 
         public bool IsReusable
---
+++ GameWeb/js/index.js
@@ -0,0 +1,38 @@
+﻿var a = document.getElementById("uname_text");
+var b = document.getElementById("pword_text");
+var main = document.getElementById("main");
+var zz = document.getElementById("zz");
+var newuser = document.getElementById("newuser");
+var newpassword = document.getElementById("newpassword");
+
+function zhu() {
+    main.style.display = "none";
+    zz.style.display = "block";
+}
+function aaaa() {
+    main.style.display = "block";
+    zz.style.display = "none";
+}
+function add() {
+    
+    $.ajax({
+        url: '/hand.ashx',
+        dataType: 'text',
+        type: 'POST',
+        data: {
+            newname: newuser.value,
+            newpass: newpassword.value,
+            method: "indexAdd",
+        },
+        success: function (out) {
+            if (out == "成功") {
+                window.alert("注册成功");
+                aaaa();
+            } else if (out == "失败") {
+                window.alert("用户名已存在");
+            }
+        },
+        error: function () { alert('error 注册'); }
+    });
+    
+}
